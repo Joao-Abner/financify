c924081cf648e4b8c3da25c1365f0d9d5bf8d12c
Joao-Abner <joaoabnerpereira@gmail.com>
2024-04-17 21:52:39 -0300

    add component "hello-world"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HelloWorldComponent } from './hello-world/hello-world.component';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,7 @@ import { RouterOutlet } from '@angular/router';
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
+  imports: [RouterOutlet, HelloWorldComponent, AloMundoComponent],
 })
 export class AppComponent {
   title = 'financify';
