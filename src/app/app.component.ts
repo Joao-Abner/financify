d5cc3327cba4aca849e7463bf34a12cbeb4f6be1
Joao-Abner <joaoabnerpereira@gmail.com>
2024-04-22 16:10:31 -0300

    init feature atividade-10

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HelloWorldComponent } from './hello-world/hello-world.component';
+import { AloMundoComponent } from './alo-mundo/alo-mundo.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'financify';
