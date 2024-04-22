7428f7dccf6f2e3fc71bf89f170ffb7a9ed984a8
Joao-Abner <joaoabnerpereira@gmail.com>
2024-04-22 17:10:17 -0300

    Feature atividade-10

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  template: ` <app-header></app-header> `,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
 })
 export class AppComponent {
   title = 'financify';
