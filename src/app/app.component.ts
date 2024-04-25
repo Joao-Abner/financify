fe9653ba89aa9a3aa53cf9786e654d7359b710ce
Joao-Abner <joaoabnerpereira@gmail.com>
2024-04-25 19:55:13 -0300

    Feature atividade-11

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -9,10 +10,25 @@ import { FooterComponent } from './footer/footer.component';
   template: ` <app-header></app-header> `,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, FormsModule],
 })
 export class AppComponent {
-  title = 'financify';
+  //Interpolation
+  title = 'Financify';
+  //---
+
+  //Property Binding  (One way)
   imageUrl =
-    'https://res.cloudinary.com/daqpjdyeu/image/upload/v1696212452/cld-sample-4.jpg';
+    'https://res.cloudinary.com/daqpjdyeu/image/upload/f_auto,q_auto/v1/financify/ijk6rt5pkbfjfnzybyry';
+  //---
+
+  //Event Binding
+  onClick() {
+    console.log('Button clicked!');
+  }
+  //---
+
+  //Two Way  Data Binding (ngModel)
+  name = 'Johnny B. Goode';
+  //---
 }
