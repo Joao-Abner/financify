e1c05c23abdef9d64a6a979459fb243dbcc84b0e
Raul F Rocha <raulferreiradarocha1@hotmail.com>
2024-07-03 00:00:08 -0300

    Update json-server links

--- src/app/pages/goals/goal.service.ts
+++ src/app/pages/goals/goal.service.ts
@@ -12,7 +12,7 @@ export interface Goal {
   providedIn: 'root',
 })
 export class GoalService {
-  private baseUrl = 'https://json-server-financify.vercel.app/users';
+  private baseUrl = 'http://localhost:3000/users';
 
   constructor(private http: HttpClient) {}
 
